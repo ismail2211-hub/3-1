8035e109f09e67ac2f6f11eb49cdd796c043dbb3
ismail2211-hub <Abdullayevo84@icloud.com>
2025-06-16 21:08:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+ 
+
+
+
+
+let min = + prompt('randomni min sonini kiriting')
+let max = + prompt('randomni max sonini kiriting')
+
+
+
+
+
+
+let randomNumber = Math.random() * (max-min) + min;
+let random = Math.ceil(randomNumber)
+
+console.log("random orkali chiqan son " + random)
+// kanketinatsiya
